4301b6c5aa55908126a9fdc73d01f95a9192df14
Shaun <ycorredius@gmail.com>
2019-07-29 06:40:24 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,20 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(cartoon)# code an argument here
   # Your code here
+  charcter = ""
+  cartoon.each_with_index{|dwarf,index| puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteers)# code an argument here
   # Your code here
+  planeteers.collect {|planeteer| planeteer = "#{planeteer.lowercase}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.collect.find do |word|
+    if word.length > 4
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
