2aa6bf0f348e5872e96c1694beed2a4601788157
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-07-31 02:39:01 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
   # Your code here
+  dwarves.each_with_index do |name,index|
+    index += 1
+    puts "#{index}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+
+def summon_captain_planet(elements)
   # Your code here
+  elements.collect do |calls|
+    calls.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  answer = false
+    calls.each do |call|
+    if call.length > 4
+      answer = true
+    end
+  end
+    answer
 end
 
-def find_the_cheese# code an argument here
+
+def find_the_cheese(cheffar_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheffar_cheese.find do |cheese|
+  cheese_types.include?(cheese)
+  end
 end
+
